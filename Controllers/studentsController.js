5849894c6d7c39426d9d026df2f2df1f5ac1d1b8
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-12-06 15:20:16 -0500

    post and put finished

--- Controllers/studentsController.js
+++ Controllers/studentsController.js
@@ -1,19 +1,23 @@
 // define routes
 
+const bodyParser = require("body-parser");
 const express = require("express");
 const router = express.Router();
+
+router.use(bodyParser.json());
+
 const pgp = require("pg-promise")();
 const db = pgp({
   connect: "postgres://pursuit:Buyenjoy20#@localhost:3333/student_dashboard",
 });
 
-router.get("/students", (req, res) => {
+router.get("/", (req, res) => {
   db.result("SELECT * FROM Students").then((allData) => {
     res.send({ students: allData.rows });
   });
 });
 
-router.get("/students/:id", (req, res) => {
+router.get("/:id", (req, res) => {
   const { id } = req.params;
   db.result(`SELECT * FROM Students WHERE students.id = $1`, [id]).then(
     (allData) => {
@@ -22,12 +26,12 @@ router.get("/students/:id", (req, res) => {
   );
 });
 
-router.post("/students", (req, res) => {
+router.post("/", (req, res) => {
   const { body } = req;
   const { first_name, last_name, email, pic, skill, company, city } = body;
 
   db.result(
-    `INSERT INTO Students (first_name, last_name, email, pic, skill, company, city) VALUES ($1, $2, $3, $4, $5, $6, $7)`,
+    `INSERT INTO Students (first_name, last_name, email, pic, skill, company, city) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`,
     [first_name, last_name, email, pic, skill, company, city]
   ).then((allData) => {
     res.send({ students: allData.rows });
@@ -36,30 +40,34 @@ router.post("/students", (req, res) => {
   console.log("POST received", req.body);
 });
 
-router.put("/students/:id", (req, res) => {
+router.put("/:id", (req, res) => {
   const { body } = req;
   const { id } = req.params;
   const { first_name, last_name, email, pic, skill, company, city } = body;
 
   db.result(
-    `UPDATE Students SET first_name=$1, last_name=$2, email=$3, pic=$4, skill=$5, company=$6, city=$7 WHERE id=$8`,
+    `UPDATE Students SET first_name=$1, last_name=$2, email=$3, pic=$4, skill=$5, company=$6, city=$7 WHERE id=$8 RETURNING *`,
     [first_name, last_name, email, pic, skill, company, city, id]
-  ).then((result) => {
-    res.send({ message: "Student updated successfully" });
+  ).then((allData) => {
+    res.send({ students: allData.rows });
   });
 
   console.log("PUT received", req.body);
 });
 
-// GET /students
-// app.get("/students", (req, res) => {
-//   try {
-//     const { students } = studentData; // take the students array from student data json file
-//     res.status(200).json({ data: students }); // return with success message and the array of students
-//   } catch (err) {
-//     res.status(500).json({ error: err.messagge });
-//   }
-// }); do not delete
+module.exports = router;
+
+/* do not delete
+
+GET /students
+app.get("/students", (req, res) => {
+  try {
+    const { students } = studentData; // take the students array from student data json file
+    res.status(200).json({ data: students }); // return with success message and the array of students
+  } catch (err) {
+    res.status(500).json({ error: err.messagge });
+  }
+}); 
 
 // GET /students/:id
 
@@ -78,4 +86,4 @@ router.get("/students/:id", (req, res) => {
   }
 });
 
-module.exports = router;
+*/
